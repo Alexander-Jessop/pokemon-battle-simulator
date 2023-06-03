d1149ab63b4606b1aacad4a007f26472bf2b7ec2
Alexander Jessop <alexanderjessop@protonmail.com>
2023-06-02 19:03:28 -0600

    Added functionality to select pokemon team and remove pokemon from team

--- Client/src/components/pokemon/PokemonCard.tsx
+++ Client/src/components/pokemon/PokemonCard.tsx
@@ -1,52 +1,47 @@
 import React from "react";
 import { PokemonType } from "../../types/PokemonType";
+
 interface Props {
-  pokemon?: PokemonType;
+  pokemon: PokemonType;
+  onSelect: (pokemon: PokemonType) => void;
+  isSelected: boolean;
 }
 
-const PokemonCard: React.FC<Props> = ({ pokemon }) => {
+const PokemonCard: React.FC<Props> = ({ pokemon, onSelect, isSelected }) => {
   const capitalize = (s: string) => {
     return s.charAt(0).toUpperCase() + s.slice(1);
   };
 
-  const renderPokemon = () => {
-    if (!pokemon) {
-      return <div>Pokemon is lost in the wild.</div>;
-    }
-
-    return (
-      <>
-        <h3 className="bg-accent-500 p-4 text-xl font-bold text-white">
-          {capitalize(pokemon.name)}
-        </h3>
-        <div className="flex justify-center">
-          <img
-            className="m-4 h-32 w-32"
-            src={pokemon.sprite}
-            alt={pokemon.name}
-          />
-        </div>
-        <div>
-          <button
-            className={`rounded bg-blue-500 px-4 py-2 font-bold
-             text-white hover:bg-blue-700`}
-          >
-            Select
-          </button>
-          <button
-            className={`rounded bg-gray-500 px-4 py-2 font-bold 
-            text-white hover:bg-gray-700`}
-          >
-            Pokedex
-          </button>
-        </div>
-      </>
-    );
+  const handleSelect = () => {
+    onSelect(pokemon);
   };
 
+  const selectButtonClassName = `rounded bg-blue-500 px-4 py-2 
+    font-bold text-white hover:bg-blue-700 ${isSelected ? "bg-blue-700" : ""}`;
+
   return (
-    <div className={" m-4 overflow-hidden rounded-lg bg-white shadow-lg"}>
-      {renderPokemon()}
+    <div className="m-4 overflow-hidden rounded-lg bg-white shadow-lg">
+      <h3 className="bg-accent-500 p-4 text-xl font-bold text-white">
+        {capitalize(pokemon.name)}
+      </h3>
+      <div className="flex justify-center">
+        <img
+          className="m-4 h-32 w-32"
+          src={pokemon.sprite}
+          alt={pokemon.name}
+        />
+      </div>
+      <div>
+        <button className={selectButtonClassName} onClick={handleSelect}>
+          {isSelected ? "Remove" : "Select"}
+        </button>
+        <button
+          className="rounded bg-gray-500 px-4 py-2
+        font-bold text-white hover:bg-gray-700"
+        >
+          Pokedex
+        </button>
+      </div>
     </div>
   );
 };
--- Client/src/components/pokemon/PokemonList.tsx
+++ Client/src/components/pokemon/PokemonList.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import { PokemonType } from "../../types/PokemonType";
-import PokemonCard from "./PokemonCard";
 import { useQuery } from "react-query";
 import { fetchPokemonList } from "../../util/fetchPokemonList";
+import PokemonCard from "./PokemonCard";
+import { PokemonType } from "../../types/PokemonType";
 
 const PokemonList: React.FC = () => {
+  const [selectedTeam, setSelectedTeam] = useState<PokemonType[]>([]);
   const [offset, setOffset] = useState<number>(0);
   const [limit, setLimit] = useState<number>(20);
 
@@ -20,7 +21,43 @@ const PokemonList: React.FC = () => {
   const handlePaginationChange = (newOffset: number, newLimit: number) => {
     if (newOffset >= 0 && newOffset <= 151) {
       setOffset(newOffset);
+      if (newLimit < limit) {
+        const selectedPokemonIds = new Set(
+          selectedTeam.map((pokemon) => pokemon.id)
+        );
+        setSelectedTeam((prevSelectedTeam) =>
+          prevSelectedTeam.filter((selectedPokemon) =>
+            selectedPokemonIds.has(selectedPokemon.id)
+          )
+        );
+      }
       setLimit(newLimit);
+    }
+  };
+
+  const handlePokemonSelect = (pokemon: PokemonType) => {
+    if (selectedTeam.length < 6) {
+      setSelectedTeam((prevSelectedTeam) => [...prevSelectedTeam, pokemon]);
+    }
+  };
+
+  const handleRemoveFromSelectedTeam = (pokemon: PokemonType) => {
+    setSelectedTeam((prevSelectedTeam) =>
+      prevSelectedTeam.filter(
+        (selectedPokemon) => selectedPokemon.id !== pokemon.id
+      )
+    );
+  };
+
+  const handleSelectOrRemove = (pokemon: PokemonType) => {
+    if (
+      selectedTeam.some((selectedPokemon) => selectedPokemon.id === pokemon.id)
+    ) {
+      handleRemoveFromSelectedTeam(pokemon);
+    } else {
+      if (selectedTeam.length < 6) {
+        handlePokemonSelect(pokemon);
+      }
     }
   };
 
@@ -34,12 +71,40 @@ const PokemonList: React.FC = () => {
     );
   }
 
+  const filteredPokemonList = pokemonList
+    ?.filter(
+      (pokemon) =>
+        !selectedTeam.some(
+          (selectedPokemon) => selectedPokemon.id === pokemon.id
+        )
+    )
+    ?.slice(0, limit);
+
   return (
     <div>
+      <div className="flex flex-wrap">
+        <h2>Selected Pokemon:</h2>
+        {selectedTeam.map((pokemon) => (
+          <div key={pokemon.id}>
+            <PokemonCard
+              pokemon={pokemon}
+              onSelect={() => handleSelectOrRemove(pokemon)}
+              isSelected={true}
+            />
+          </div>
+        ))}
+      </div>
       <h1>Pokemon List</h1>
       <div className="m-10 flex flex-wrap justify-center">
-        {pokemonList?.map((pokemon) => (
-          <PokemonCard pokemon={pokemon} key={pokemon.id} />
+        {filteredPokemonList?.map((pokemon) => (
+          <PokemonCard
+            pokemon={pokemon}
+            key={pokemon.id}
+            onSelect={() => handleSelectOrRemove(pokemon)}
+            isSelected={selectedTeam.some(
+              (selectedPokemon) => selectedPokemon.id === pokemon.id
+            )}
+          />
         ))}
       </div>
       <div>
@@ -52,7 +117,7 @@ const PokemonList: React.FC = () => {
         <select
           value={limit}
           onChange={(e) =>
-            handlePaginationChange(offset, parseInt(e.target.value))
+            handlePaginationChange(offset, +e.target.value as number)
           }
         >
           <option value={10}>10</option>
