24f8110796d7bbb6030eb1b73b0b6ceb60aaa3eb
Alexander Jessop <alexanderjessop@protonmail.com>
2023-07-03 14:27:35 -0600

    changed proxy

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -1,15 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 export default defineConfig(() => {
   return {
     plugins: [react()],
     server: {
       proxy: {
-        "/api/": "http://localhost:3000",
+        "/api/": "https://pokemon-battle-simulator-lake.vercel.app",
       },
     },
     define: {},
