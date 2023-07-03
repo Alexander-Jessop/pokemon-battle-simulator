9d520a33a095c13ee4e617f69948ae23e71f9da2
Alexander Jessop <alexanderjessop@protonmail.com>
2023-07-03 14:35:02 -0600

    removed proxy

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -4,11 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(() => {
   return {
     plugins: [react()],
-    server: {
-      proxy: {
-        "/api/": "https://pokemon-battle-simulator-lake.vercel.app",
-      },
-    },
     define: {},
   };
 });
